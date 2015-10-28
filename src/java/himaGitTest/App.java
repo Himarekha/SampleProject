4c189dd63ac6854acc4639a4033eef451df20145
kalyan88 <kalyanisno1@gmail.com>
2015-10-27 21:02:33 -0400

    <Duplicated code 10/27>

--- src/java/himaGitTest/App.java
+++ src/java/himaGitTest/App.java
@@ -21,5 +21,25 @@ public class App
        /**TEstting*/
         
         System.out.println( " Commit from KG Machine added more code" );
+        
+        System.out.println( "Hello World!" );
+        
+        System.out.println( "Git testing cycle 2" );
+        
+        System.out.println( " https://github.com/Himarekha/SampleProject.git" );
+        
+        System.out.println( " Commit from KG Machine" );
+       /**TEstting*/
+        
+        System.out.println( " Commit from KG Machine added more code" );System.out.println( "Hello World!" );
+        
+        System.out.println( "Git testing cycle 2" );
+        
+        System.out.println( " https://github.com/Himarekha/SampleProject.git" );
+        
+        System.out.println( " Commit from KG Machine" );
+       /**TEstting*/
+        
+        System.out.println( " Commit from KG Machine added more code" );
     }
 }
